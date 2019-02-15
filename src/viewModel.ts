f4b8e794cd0c4fea05f8574b6c23729cf2ccc86a
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-02-16 09:50:09 +1300

    Added view model interface declaration (IViewModel)

--- src/viewModel.ts
+++ src/viewModel.ts
@@ -1,3 +1,16 @@
 module powerbi.extensibility.visual {
 
+    /**
+     *  Everything we need to render our visual
+     * 
+     *  @property {VisualSettings} settings         - Parsed visual settings
+     *  @property {IDimensions} dimensions          - Dimensions of the visual container
+     *  @property {ICard} card                      - Card configuration and logic
+     */
+        export interface IViewModel {
+            settings: VisualSettings;
+            dimensions: IDimensions;
+            card: ICard;
+        }
+
 }
